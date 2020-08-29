4eb35e2ae898ef30b212894872a48a55b18667f7
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-29 17:15:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   
   def add_item(item, price, quantity=1)
     quantity.times {self.items << item}
-    self.transactions << price * quantity
+    self.last_transaction = price * quantity
     self.total += self.transactions.last
   end
   
